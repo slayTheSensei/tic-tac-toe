136d600c3290e083650820bdd6397dbd0e14cce2
slayTheSensei <kylejpearson@gmail.com>
2017-08-01 13:12:54 -0400

    WIP
    Added CSS Game Board using flexbox.

---
+++ assets/scripts/main.js
@@ -0,0 +1,16 @@
+// All possible winning moves
+
+winList = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+$('square0').click(function () {
+  $('square0').text().appendTo('X')
+})
