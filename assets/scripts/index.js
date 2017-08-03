1c49e859fcb65ba34cb8cb21329f50805bc3195e
slayTheSensei <kylejpearson@gmail.com>
2017-08-03 16:44:48 -0400

    FINISHED GAME LOGIC
    Working on API

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -14,42 +14,54 @@ $(() => {
 require('./example')
 
 /// //////////////////////////
-/// / GAME LOGIC /////////////
+/// / ARRAYS /////////////
 /// //////////////////////////
 
-// const winList = [
-//   [0, 1, 2],
-//   [3, 4, 5],
-//   [6, 7, 8],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [0, 4, 8],
-//   [2, 4, 6]
-// ]
-//
+const grid = []
+const oWins = []
+const xWins = []
 
-/// //////////////////////////
-/// / Turn Functionality /////
-/// //////////////////////////
-
+// Selects all elements with the class 'item'
+const squares = $('.item')
 let turn = 0
+
+/// //////////////////////////
+/// / TURN FUNCTIONALITY /////
+/// //////////////////////////
+const empty = function () {
+
+}
 
 // Test for odd or even and display current marker
 const render = function () {
-  turn += 1
   if (isEven() === true) {
+    nextTurn()
     console.log('X Turn')
     $('.item').click(currentX)
   } else {
-    console.log('o Turn')
+    nextTurn()
+    console.log('O Turn')
     $('.item').on('click', currentO)
   }
+  checkForWin()
+  // TODO add a callback function to notify player of turn change
 }
 
 /// //////////////////////////
 /// / Callbacks /////
 /// //////////////////////////
+
+const xIndex = function () {
+  let currentIndex = squares.index(this)
+  // if ($(squares[currentIndex]).text('X')) {
+  xWins.push(currentIndex)
+}
+
+const oIndex = function () {
+  let currentIndex = squares.index(this)
+  // if ($(squares[currentIndex]).text('O')) {
+  oWins.push(currentIndex)
+}
 
 // Changes turn
 const nextTurn = function () {
@@ -62,17 +74,119 @@ function isEven () {
   return turn % 2 === 0
 }
 
-// Changes marker to O
-function currentX () {
-  $(this).text('X')
+const playerIcon = function () {
+
 }
 
 // Changes marker to X
-function currentO () {
-  $(this).text('O')
+function currentX () {
+  $(this).text('X').off()
 }
 
-// function isOdd(n) {
-//   return Math.abs(n % 2) === 1
-// }
+// Changes marker to O
+function currentO () {
+  $(this).text('O').off()
+}
+
+// Pushes all grid elements in the 'grid' array
+const board = function () {
+  for (let i = 0; i < squares.length; i++) {
+    grid.push(squares[i])
+  }
+  console.log(grid)
+  return grid
+  // TODO push the element being clicked into the win array
+}
+
+// ///////////////////////
+// ///// WIN LOGIC ///////
+// ///////////////////////
+
+const checkForWin = function () {
+  let won = 0
+  if ($('#0').text() === ('X') && $('#1').text() === ('X') && $('#2').text() === 'X') {
+    won = 1
+    console.log('X Wins!')
+    $('.item').off()
+
+  } else if ($('#0').text() === ('O') && $('#1').text() === ('O') && $('#2').text() === 'O') {
+    won = 1
+    console.log('O Wins!')
+    $('.item').off()
+
+  } else if ($('#3').text() === ('X') && $('#4').text() === ('X') && $('#5').text() === 'X') {
+    won = 1
+    console.log('X Wins!')
+    $('.item').off()
+
+  } else if ($('#3').text() === ('O') && $('#4').text() === ('O') && $('#5').text() === 'O') {
+    won = 1
+    console.log('O Wins!')
+    $('.item').off()
+
+  } else if ($('#6').text() === ('X') && $('#7').text() === ('X') && $('#8').text() === 'X') {
+    won = 1
+    console.log('X Wins!')
+    $('.item').off()
+
+  } else if ($('#6').text() === ('O') && $('#7').text() === ('O') && $('#8').text() === 'O') {
+    won = 1
+    console.log('O Wins!')
+    $('.item').off()
+
+  } else if ($('#0').text() === ('X') && $('#3').text() === ('X') && $('#6').text() === 'X') {
+    won = 1
+    console.log('X Wins!')
+    $('.item').off()
+
+  } else if ($('#0').text() === ('O') && $('#3').text() === ('O') && $('#6').text() === 'O') {
+    won = 1
+    console.log('O Wins!')
+    $('.item').off()
+
+  } else if ($('#1').text() === ('X') && $('#4').text() === ('X') && $('#7').text() === 'X') {
+    won = 1
+    console.log('X Wins!')
+    $('.item').off()
+
+  } else if ($('#1').text() === ('O') && $('#4').text() === ('O') && $('#7').text() === 'O') {
+    won = 1
+    console.log('O Wins!')
+    $('.item').off()
+
+  } else if ($('#2').text() === ('X') && $('#5').text() === ('X') && $('#8').text() === 'X') {
+    won = 1
+    console.log('X Wins!')
+
+  } else if ($('#2').text() === ('O') && $('#5').text() === ('O') && $('#8').text() === 'O') {
+    won = 1
+    console.log('O Wins!')
+    $('.item').off()
+
+  } else if ($('#0').text() === ('X') && $('#4').text() === ('X') && $('#8').text() === 'X') {
+    won = 1
+    console.log('X Wins!')
+    $('.item').off()
+
+  } else if ($('#0').text() === ('O') && $('#4').text() === ('O') && $('#8').text() === 'O') {
+    won = 1
+    console.log('O Wins!')
+    $('.item').off()
+
+  } else if ($('#2').text() === ('X') && $('#4').text() === ('X') && $('#6').text() === 'X') {
+    won = 1
+    console.log('X Wins!')
+    $('.item').off()
+
+  } else if ($('#2').text() === ('O') && $('#4').text() === ('O') && $('#6').text() === 'O') {
+    won = 1
+    console.log('O Wins!')
+    $('.item').off()
+  }
+}
+
+// ////////////////////////////
+// ///// FUNCTION CALLS ///////
+// ////////////////////////////
+render()
 $('.item').on('click', render)
