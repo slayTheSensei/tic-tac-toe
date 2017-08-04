66e4afb88d33aa3ff3ee66f3da3b2304372886bb
slayTheSensei <kylejpearson@gmail.com>
2017-08-04 02:01:53 -0400

    WIP - COMPLETED MOST OF LOGIC
    Still need to complete api and finishing touches. Ran out of time

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -25,6 +25,13 @@ const xWins = []
 const squares = $('.item')
 let turn = 0
 
+const draw = function () {
+  if (turn > 9) {
+    console.log('Draw!')
+    return $('.item').off
+  }
+}
+
 /// //////////////////////////
 /// / TURN FUNCTIONALITY /////
 /// //////////////////////////
@@ -37,12 +44,16 @@ const render = function () {
   if (isEven() === true) {
     nextTurn()
     console.log('X Turn')
+    draw()
     $('.item').click(currentX)
   } else {
     nextTurn()
     console.log('O Turn')
+    draw()
     $('.item').on('click', currentO)
   }
+  $('#myTurnO').toggleClass('shade')
+  $('#myTurnX').toggleClass('shade')
   checkForWin()
   // TODO add a callback function to notify player of turn change
 }
@@ -51,17 +62,17 @@ const render = function () {
 /// / Callbacks /////
 /// //////////////////////////
 
-const xIndex = function () {
-  let currentIndex = squares.index(this)
-  // if ($(squares[currentIndex]).text('X')) {
-  xWins.push(currentIndex)
-}
-
-const oIndex = function () {
-  let currentIndex = squares.index(this)
-  // if ($(squares[currentIndex]).text('O')) {
-  oWins.push(currentIndex)
-}
+// const xIndex = function () {
+//   let currentIndex = squares.index(this)
+//   // if ($(squares[currentIndex]).text('X')) {
+//   xWins.push(currentIndex)
+// }
+//
+// const oIndex = function () {
+//   let currentIndex = squares.index(this)
+//   // if ($(squares[currentIndex]).text('O')) {
+//   oWins.push(currentIndex)
+// }
 
 // Changes turn
 const nextTurn = function () {
@@ -89,98 +100,98 @@ function currentO () {
 }
 
 // Pushes all grid elements in the 'grid' array
-const board = function () {
-  for (let i = 0; i < squares.length; i++) {
-    grid.push(squares[i])
-  }
-  console.log(grid)
-  return grid
-  // TODO push the element being clicked into the win array
-}
+// const board = function () {
+//   for (let i = 0; i < squares.length; i++) {
+//     grid.push(squares[i])
+// /
+//   // TODO push the element being clicked into the win array
+// }
 
 // ///////////////////////
 // ///// WIN LOGIC ///////
 // ///////////////////////
 
 const checkForWin = function () {
-  let won = 0
   if ($('#0').text() === ('X') && $('#1').text() === ('X') && $('#2').text() === 'X') {
-    won = 1
     console.log('X Wins!')
+$("#btn-popup01").click();
     $('.item').off()
 
   } else if ($('#0').text() === ('O') && $('#1').text() === ('O') && $('#2').text() === 'O') {
-    won = 1
     console.log('O Wins!')
+    $("#btn-popup02").click();
     $('.item').off()
 
   } else if ($('#3').text() === ('X') && $('#4').text() === ('X') && $('#5').text() === 'X') {
-    won = 1
     console.log('X Wins!')
+    $("#btn-popup01").click();
     $('.item').off()
 
   } else if ($('#3').text() === ('O') && $('#4').text() === ('O') && $('#5').text() === 'O') {
-    won = 1
     console.log('O Wins!')
+    $("#btn-popup02").click();
     $('.item').off()
 
   } else if ($('#6').text() === ('X') && $('#7').text() === ('X') && $('#8').text() === 'X') {
-    won = 1
     console.log('X Wins!')
+    $("#btn-popup01").click();
     $('.item').off()
 
   } else if ($('#6').text() === ('O') && $('#7').text() === ('O') && $('#8').text() === 'O') {
-    won = 1
     console.log('O Wins!')
+    $("#btn-popup02").click();
     $('.item').off()
 
   } else if ($('#0').text() === ('X') && $('#3').text() === ('X') && $('#6').text() === 'X') {
-    won = 1
     console.log('X Wins!')
+    $("#btn-popup01").click();
     $('.item').off()
 
   } else if ($('#0').text() === ('O') && $('#3').text() === ('O') && $('#6').text() === 'O') {
-    won = 1
     console.log('O Wins!')
+    $("#btn-popup02").click();
     $('.item').off()
 
   } else if ($('#1').text() === ('X') && $('#4').text() === ('X') && $('#7').text() === 'X') {
-    won = 1
     console.log('X Wins!')
+    $("#btn-popup01").click();
     $('.item').off()
 
   } else if ($('#1').text() === ('O') && $('#4').text() === ('O') && $('#7').text() === 'O') {
-    won = 1
     console.log('O Wins!')
+    $("#btn-popup02").click();
     $('.item').off()
 
   } else if ($('#2').text() === ('X') && $('#5').text() === ('X') && $('#8').text() === 'X') {
-    won = 1
     console.log('X Wins!')
+    $("#btn-popup01").click();
+    $('.item').off()
 
   } else if ($('#2').text() === ('O') && $('#5').text() === ('O') && $('#8').text() === 'O') {
-    won = 1
     console.log('O Wins!')
+    $("#btn-popup02").click();
     $('.item').off()
 
   } else if ($('#0').text() === ('X') && $('#4').text() === ('X') && $('#8').text() === 'X') {
-    won = 1
     console.log('X Wins!')
+    $("#btn-popup01").click();
     $('.item').off()
 
   } else if ($('#0').text() === ('O') && $('#4').text() === ('O') && $('#8').text() === 'O') {
-    won = 1
+
     console.log('O Wins!')
+    $("#btn-popup02").click();
     $('.item').off()
 
+// TODO Fix whatever is broken
   } else if ($('#2').text() === ('X') && $('#4').text() === ('X') && $('#6').text() === 'X') {
-    won = 1
     console.log('X Wins!')
+    $("#btn-popup01").click();
     $('.item').off()
 
   } else if ($('#2').text() === ('O') && $('#4').text() === ('O') && $('#6').text() === 'O') {
-    won = 1
     console.log('O Wins!')
+    $("#btn-popup02").click();
     $('.item').off()
   }
 }
@@ -190,3 +201,26 @@ const checkForWin = function () {
 // ////////////////////////////
 render()
 $('.item').on('click', render)
+
+// ////////////////////////////
+// ///// FUNCTION CALLS ///////
+// ////////////////////////////
+
+$(document).ready(function(){
+    $("body").on("click", "button.popup", function(){
+      var btnId = $(this).attr('id');
+      $( "body" ).addClass( "popup-open" ).fadeIn(2000);
+      $( "."+ btnId ).addClass( "popup-open" ).fadeIn(400);
+    }).on('click', 'button.link', function(){
+      var btnFormAction = $(this).attr('formaction');
+      var btnTarget = $(this).attr('formtarget');
+      window.open(btnFormAction, btnTarget);
+    });
+
+// close/hide popup (+overlay)
+    $.fn.popupClose = function() {
+      $( "body" ).removeClass( "popup-open" )
+      $( ".overlay_popup" ).removeClass("popup-open");
+      return this;
+    };
+});
